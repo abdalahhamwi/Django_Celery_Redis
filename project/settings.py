4c88610d57f930be6b3d072c3dd139efc27e70da
abdala hamwi <abdalah09941@gmail.com>
2026-02-04 13:42:13 +0300

    fix bug

--- project/settings.py
+++ project/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # CELERY_RESULT_BACKEND = "redis://localhost:6379"
 # CELERY_BROKER_URL = "redis://localhost:6379"
 
-# celery + docker
+# celery + redis + docker
 
 CELERY_RESULT_BACKEND = "redis://redis:6379"
 CELERY_BROKER_URL = "redis://redis:6379"
